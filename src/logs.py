8e88652298a89357c65db3b10852d0fa06291caf
Willy <19799671+Willy-C@users.noreply.github.com>
2022-08-17 01:53:48 -0400

    Update logging to use logging module

--- src/logs.py
+++ src/logs.py
@@ -1,28 +1,41 @@
-import glob
-import os
-import time
+import pathlib
+import logging
+from logging.handlers import RotatingFileHandler
 
-class Logging:
+
+class LoggingClass:
     def __init__(self):
         self.logFileOpened = False
+        self.logger = logging.getLogger()
+        self.logger.setLevel(logging.INFO)
 
-    def log(self, stringToLog: str):
-        # creating logs folder
-        try:
-            os.mkdir(os.getcwd() + "\logs")
-        except FileExistsError:
-            pass
-        filenames = []
-        for filename in glob.glob(r"logs/log-*.txt"):
-            filenames.append(int(filename[9:-4]))
-        if len(filenames) == 0:
-            filenames.append(0)
-        if self.logFileOpened:
-            with open(f"logs/log-{max(filenames)}.txt", "a") as logFile:
-                logFile.write(f"[{time.strftime('%Y.%m.%d-%H.%M.%S', time.localtime(time.time()))}]"
-                              f" {stringToLog.encode('ascii', 'replace').decode()}\n")
-        else:
-            with open(f"logs/log-{max(filenames) + 1}.txt", "w") as logFile:
-                self.logFileOpened = True
-                logFile.write(f"[{time.strftime('%Y.%m.%d-%H.%M.%S', time.localtime(time.time()))}]"
-                              f" {stringToLog.encode('ascii', 'replace').decode()}\n")
+        filename = 'vry.log'
+
+        max_bytes = 10 * 1024 * 1024  # 10 MiB
+        handler = RotatingFileHandler(
+            filename=filename,
+            encoding='utf-8',
+            mode='w',
+            maxBytes=max_bytes,
+            backupCount=10,
+            delay=True  # We set delay=True to avoid generating an empty file if we rollover early
+        )
+        # We do a rollover at the start so new instances start writing in their own files
+        # https://stackoverflow.com/questions/44635896/how-to-create-a-new-log-file-every-time-the-application-runs
+        if pathlib.Path(filename).is_file():
+            handler.doRollover()
+
+        dt_fmt = '%Y-%m-%d %H:%M:%S'
+        formatter = logging.Formatter('[{asctime}] [{levelname}]: {message}', dt_fmt, style='{')
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+
+    def log(self, message: str):
+        """We don't really need any extensive logs, so we will just throw everything under INFO"""
+        self.logger.info(message)
+
+    def error(self, message: str):
+        self.logger.error(message, exc_info=True)
+
+
+Logging = LoggingClass()
